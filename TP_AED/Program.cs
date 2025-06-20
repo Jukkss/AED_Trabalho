8e3a5b929255c72699e5fbd83f93dac4deb2db1e
João Victor Soares Souza <sjuao7729@gmail.com>
2025-06-20 20:19:45 -0300

    Remoção do quick

--- TP_AED/Program.cs
+++ TP_AED/Program.cs
@@ -51,6 +51,8 @@ namespace TP_AED
                     linha = lerEntrada.ReadLine();
                     string[] candidato = linha.Split(';');
                     candidatosVet[i] = new Candidato(candidato[0], double.Parse(candidato[1]), double.Parse(candidato[2]), double.Parse(candidato[3]), int.Parse(candidato[4]), int.Parse(candidato[5]));
+                    //Soares
+                    candidatosVet[i].IndiceOriginal = i;
                 }
                 lerEntrada.Close();
 
@@ -141,11 +143,15 @@ namespace TP_AED
         // Soares
         static int CompararCandidatos(Candidato a, Candidato b)
         {
-            int cmpMedia = a.Media.CompareTo(b.Media);
+            int cmpMedia = b.Media.CompareTo(a.Media);
             if (cmpMedia != 0)
                 return cmpMedia;
 
-            return a.Notas[0].CompareTo(b.Notas[0]);
+            int cmpNotaRed = b.Notas[0].CompareTo(a.Notas[0]); 
+            if (cmpNotaRed != 0)
+                return cmpNotaRed;
+
+            return a.IndiceOriginal.CompareTo(b.IndiceOriginal); 
         }
         // Soares
         static void Trocar(Candidato[] vet, int i, int j)
@@ -194,7 +200,7 @@ namespace TP_AED
                 foreach (Candidato c in selecionados)
                     sw.WriteLine($"{c.Nome} {c.Media.ToString("n1")}");
 
-                sw.WriteLine("Fila de Espera:");
+                sw.WriteLine("Fila de Espera:");    
                 foreach (Candidato c in filaEspera)
                     sw.WriteLine($"{c.Nome} {c.Media.ToString("n1")}");
 
